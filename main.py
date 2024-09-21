a88071d2d7b2723a1f739888039da096d2a65061
ivanvaganov <iv@nosota.com>
2024-09-21 22:02:34 +0300

    added exception handling

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import logging
+
 import pandas as pd
 from tqdm import tqdm
 from db import wait_pgsql_connection, query_smsd_orders
 from predict import predict_tariff
 from models.ai.processing import prepare_prices
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     wait_pgsql_connection()
@@ -16,22 +20,25 @@ if __name__ == '__main__':
                                     'real_price', 'predicted_price', 'inaccuracy'])
 
     for index, row in tqdm(df.iterrows(), total=len(df)):
-        region_code = row['region_code']
-        distance = row['distance']
-        crop_id = row['crop_id']
-        order_date = row['order_date']
-        to_address = row['dest_title']
-        today_date = order_date
+        try:
+            region_code = row['region_code']
+            distance = row['distance']
+            crop_id = row['crop_id']
+            order_date = row['order_date']
+            to_address = row['dest_title']
+            today_date = order_date
 
-        predicted_price = predict_tariff(pr_df, region_code, today_date, order_date, crop_id, distance, to_address)
-        real_price = float(row['price_gross_per_ton']) // 100
+            predicted_price = predict_tariff(pr_df, region_code, today_date, order_date, crop_id, distance, to_address)
+            real_price = float(row['price_gross_per_ton']) // 100
 
-        disassembled_weight = row['disassembled_weight']
-        hours_until_disassembled = row['hours_until_disassembled']
-        crop_name = row['crop_name']
+            disassembled_weight = row['disassembled_weight']
+            hours_until_disassembled = row['hours_until_disassembled']
+            crop_name = row['crop_name']
 
-        results.loc[len(df)] = [region_code, order_date, distance, crop_name, disassembled_weight, hours_until_disassembled,
-                           real_price, predicted_price, predicted_price - real_price]
+            results.loc[len(df)] = [region_code, order_date, distance, crop_name, disassembled_weight, hours_until_disassembled,
+                               real_price, predicted_price, predicted_price - real_price]
+        except Exception as ex:
+            logger.exception(ex)
 
     results_sorted = results.sort_values(by='distance')
     results_sorted.to_excel("out/Результаты проверки формулы.xlsx", index=False)  # index=False prevents saving the index
